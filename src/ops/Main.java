2b6427638e68f40eefa6b484416f7a379d2201fd
Victoria <vriani@centronelson.org>
2022-04-06 12:08:05 +0200

    Subimos el proyecto actualizado con opción de añadir más números

--- src/ops/Main.java
+++ src/ops/Main.java
@@ -1,36 +1,41 @@
 package ops;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		Scanner sn = new Scanner(System.in);
 		int opt = 0;
+		int oper = 0;
 		
 		do {
 			mostrarMenu();
-			opt = sc.nextInt();
+			opt = sn.nextInt();
 			switch(opt) {
 				case 1:
-					int num1 = 0;
-					int num2 = 0;
+					System.out.println("¿Con cuántos números vas a operar?");
+					oper = sn.nextInt();
+					int numeros[] = new int[oper];
 					
-					System.out.println("Introduce un número: ");
-					num1 = sc.nextInt();
-					System.out.println("Introduce otro número: ");
-					num2 = sc.nextInt();
-					
-					sumar2(num1, num2);
+					for (int i = 0; i < numeros.length; i++) {
+						System.out.println("Introduce un número: ");
+						numeros[i] = sn.nextInt();
+					}
+					sumar2(numeros);
 					break;
 				case 2:
-					System.out.println("Introduce un número: ");
-					num1 = sc.nextInt();
-					System.out.println("Introduce otro número: ");
-					num2 = sc.nextInt();
+					System.out.println("¿Con cuántos números vas a operar?");
+					oper = sn.nextInt();
+					int numerosMult[] = new int[oper];
 					
-					multiplicar2(num1, num2);
+					for (int i = 0; i < numerosMult.length; i++) {
+						System.out.println("Introduce un número: ");
+						numerosMult[i] = sn.nextInt();
+					} 
+					multiplicar2(numerosMult);
 					break;
 				case 3:
 					System.out.println("Se saldrá del programa.");
@@ -47,13 +52,19 @@ public class Main {
 		System.out.println("------------------------------------");
 	}
 	
-	public static void sumar2(int num1, int num2) {
-		int resultado = num1 + num2;
+	public static void sumar2(int[] num) {
+		int resultado = 0;
+		for (int i = 0; i < num.length; i++) {
+			resultado += num[i];
+		}
 		System.out.println("El resultado es " + resultado);
 	}
 	
-	public static void multiplicar2(int num1, int num2) {
-		int resultado = num1 * num2;
+	public static void multiplicar2(int[] num) {
+		int resultado = 1;
+		for (int i = 0; i < num.length; i++) {
+			resultado *= num[i];
+		}
 		System.out.println("El resultado es " + resultado);
 	}
 }
